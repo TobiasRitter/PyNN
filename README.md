ddf601c1a8c4f2a18c8e566428ee3e3d30c7cdb5
TobiasRitter <tobias.ritter98@gmail.com>
2021-01-07 19:34:39 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# PyNN
+
+This project aims to implement a fully functional Multi-Layer Perceptron. It relies on the numpy library. The project should point out the fundamental concepts of Artifical Neural Networks, such as forward and backward propagation.
---
+++ layer.py
@@ -0,0 +1,22 @@
+class Layer:
+    def forward():
+        pass
+
+    def backward():
+        pass
+
+
+class Relu_Activation(Layer):
+    def forward():
+        pass
+
+    def backward():
+        pass
+
+
+class Sigmoid_Activation(Layer):
+    def forward():
+        pass
+
+    def backward():
+        pass
